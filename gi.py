55bb96fa8c8f28bada2cad941847e21e66cae15b
abhishekbh0476 <abhishekbh0476@gmail.com>
2022-11-14 14:24:10 +0530

    change1

---
+++ gi.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:15:23 2022
+
+@author: cse
+"""
+
+no_of_units=50
+mass_of_sum=1.989e30
+leng_china=21196
+print(no_of_units)
+print(mass_of_sum)
+print(leng_china)
